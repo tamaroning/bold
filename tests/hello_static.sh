3f36738d8346659010ea87f8edc185fb2ae75fcd
Raiki Tamura <tamaron1203@gmail.com>
2024-01-14 16:48:52 +0900

    Add more tests

---
+++ tests/hello_static.sh
@@ -0,0 +1,24 @@
+cat <<EOF | cc -o %hello.o -c -x assembler -
+  .text
+  .globl main
+main:
+  lea msg(%rip), %rdi
+  xor %rax, %rax
+  call printf
+  xor %rax, %rax
+  ret
+
+  .data
+msg:
+  .string "Hello world\n"
+EOF
+
+cargo run /usr/lib/x86_64-linux-gnu/crt1.o \
+  /usr/lib/x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/9/crtbeginT.o \
+  /usr/lib/gcc/x86_64-linux-gnu/9/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/9/libgcc_eh.a \
+  /usr/lib/x86_64-linux-gnu/libc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/9/crtend.o \
+  /usr/lib/x86_64-linux-gnu/crtn.o \
+  %hello.o
